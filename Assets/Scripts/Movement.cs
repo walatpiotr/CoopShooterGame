2eb005bd7db87325b3ca5955247d0a0603713654
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-01-02 01:23:45 +0100

    temp enemy movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float accelerationTime = 2f;
+     public float maxSpeed = 5f;
+     private Vector2 movement;
+     private float timeLeft;
+     public Rigidbody2D rb;
+
+     void Start () {
+         rb = GetComponent<Rigidbody2D> ();
+ 
+     }
+     
+     void Update()
+     {
+       timeLeft -= Time.deltaTime;
+       if(timeLeft <= 0)
+       {
+         movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+         timeLeft += accelerationTime;
+       }
+     }
+     
+     void FixedUpdate()
+     {
+       rb.AddForce(movement * maxSpeed);
+     }
+}
