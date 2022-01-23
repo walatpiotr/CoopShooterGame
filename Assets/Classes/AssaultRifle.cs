521f03ef299709565ae968861b2ef19d29aa2d88
Piotr Walat <pwalat98@gmail.com>
2022-01-23 19:21:54 +0100

    added AssaultRifle class

---
+++ Assets/Classes/AssaultRifle.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Classes
+{
+    public static class AssaultRifle
+    {
+        public static float fireRate = 0.2f;
+        public static float bulletForce = 35f;
+        public static int magazineSize = 30;
+        public static float accuracyAngle = 3f;
+        public static float damage = 30f;
+    }
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using Assets.Classes;
 
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
 
-    public float bulletForce = 20f;
-
     private bool isShooting=false;
-
-    private float shootingRate = 0.2f;
-
     private float timer = 0f;
+
+    public float fireRate;
+    public float bulletForce;
+    public int magazineSize;
+    public float accuracyAngle;
+    public float damage;
+
+    private void Awake()
+    {
+        Configure("rifle");    
+    }
 
     // Update is called once per frame
     void Update()
@@ -34,21 +41,38 @@ public class Shooting : MonoBehaviour
             if (timer <= 0f)
             {
                 Shoot();
-                timer = shootingRate;
+                timer = fireRate;
             }
         }
     }
 
     void Shoot()
     {
-        var tiltAngle = Random.Range(-1f, 1f);
+        var tiltAngle = Random.Range(-accuracyAngle, accuracyAngle);
 
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<LiveScript>().dmg = damage;
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 
         bullet.transform.Rotate(0f, 0f, tiltAngle);
         rb.SetRotation(Quaternion.Euler(0f, 0f, tiltAngle));
 
         rb.AddForce(rb.transform.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    private void Configure(string gun)
+    {
+        switch (gun)
+        {
+            case "rifle":
+                accuracyAngle = AssaultRifle.accuracyAngle;
+                bulletForce =  AssaultRifle.bulletForce;
+                fireRate = AssaultRifle.fireRate;
+                magazineSize = AssaultRifle.magazineSize;
+                damage = AssaultRifle.damage;
+                Debug.Log("configured Rifle");
+                break;
+            // TODO add new cases
+        }
     }
 }
