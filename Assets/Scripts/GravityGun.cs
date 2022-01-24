a5ed5c593a09c6fdce4ca54e584d8c1a5abdf6eb
Piotr Walat <pwalat98@gmail.com>
2022-01-25 00:38:53 +0100

    gravity gun WIP

--- Assets/Scripts/GravityGun.cs
+++ Assets/Scripts/GravityGun.cs
@@ -25,17 +25,24 @@ public class GravityGun : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             grabObstacle = false;
+            //obstacleToGrab.transform.parent = null;
+            obstacleToGrab = null;
         }
         MoveObstacle();
     }
 
     private void MoveObstacle()
     {
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //mousePosition = cam.ViewportToWorldPoint(Input.mousePosition);
+        Debug.Log(transform.position + " + " + mousePosition);
+
         if (grabObstacle)
         {
-            // TODO refactor bcz it goes on z axis (?)
-            obstacleToGrab.transform.position = Vector3.MoveTowards(obstacleToGrab.transform.position, cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f)), 0f);
-            obstacleToGrab.transform.rotation = transform.rotation;
+            if (obstacleToGrab)
+            {
+                obstacleToGrab.transform.position = mousePosition;
+            }
         }
     }
 }
--- Assets/Scripts/GravityPossibilityScript.cs
+++ Assets/Scripts/GravityPossibilityScript.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityPossibilityScript : MonoBehaviour
 {
     public Transform grabSprite;
-    
+
+    public List<SpriteRenderer> spritesEnabled;
+
     void Update()
     {
         CheckIfPlayerNearby();
@@ -13,15 +16,32 @@ public class GravityPossibilityScript : MonoBehaviour
     {
         var obstacles = GameObject.FindGameObjectsWithTag("obstacle");
 
-        // grabSprite.enabled = false;
-
-        foreach (var obstacle in obstacles)
+        if(transform.GetComponent<GravityGun>().obstacleToGrab == null)
         {
-            if (Vector2.Distance(obstacle.transform.position, transform.position) < 3f)
+            // grabSprite.enabled = false;
+
+            foreach (var obstacle in obstacles)
             {
-                grabSprite = obstacle.transform.Find("GravityGunEnablePopUp");
-                grabSprite.GetComponent<SpriteRenderer>().enabled = true;
-                transform.GetComponent<GravityGun>().obstacleToGrab = obstacle;
+                if (Vector2.Distance(obstacle.transform.position, transform.position) < 3f)
+                {
+                    grabSprite = obstacle.transform.Find("GravityGunEnablePopUp");
+                    grabSprite.GetComponent<SpriteRenderer>().enabled = true;
+                    spritesEnabled.Add(grabSprite.GetComponent<SpriteRenderer>());
+                    if (Input.GetKeyDown(KeyCode.Mouse1))
+                    { transform.GetComponent<GravityGun>().obstacleToGrab = obstacle; }
+                }
+            }
+        }
+
+        if(spritesEnabled.Count > 0)
+        {
+            foreach (var sprite in spritesEnabled)
+            {
+                if (Vector2.Distance(sprite.transform.position, transform.position) >= 3f)
+                {
+                    sprite.enabled = false;
+                    spritesEnabled.Remove(sprite);
+                }
             }
         }
     }
