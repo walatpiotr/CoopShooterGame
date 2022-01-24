1a2d6e0a7dd87abaeea5108865732dadfca8e334
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-01-24 22:34:27 +0100

    add gun sprite to UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -11,21 +11,26 @@ public class UI : MonoBehaviour
     public float mags;
     public TMP_Text textbox;
     GameObject player;
+    public string currentWeapon;
+    public Image img;
     void Start()
     {
 
         textbox = GetComponentInChildren<TMP_Text>();
         player=GameObject.FindWithTag("Player");
-        
+        img=GetComponentInChildren<Image>();
         
     }
 
     void Update()
     {
+        currentWeapon=player.GetComponent<Shooting>().currentWeapon;
+
         health=player.GetComponent<HealthBar>().curHealth;
         ammo=player.GetComponent<Shooting>().currentMagazine;
-        mags=player.GetComponent<Shooting>().magazines[player.GetComponent<Shooting>().currentWeapon].Item2;
+        mags=player.GetComponent<Shooting>().magazines[currentWeapon].Item2;
 
         textbox.text = "Health:"+health+"  Ammo:"+ammo+"  Magazines:"+mags;
+        img.sprite=Resources.Load<Sprite>("Guns/"+currentWeapon);
     }
 }
